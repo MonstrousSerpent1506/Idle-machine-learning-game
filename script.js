4af552a17b6c6ae062d9b90c7eb8157819f360e6
MonstrousSerpent1506 <106156763+MonstrousSerpent1506@users.noreply.github.com>
2023-11-25 09:21:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-const generateDataButton = document.getElementById('generate-data');
-const dataPointsCountElement = document.getElementById('data-points-count');
-const modelAccuracyElement = document.getElementById('model-accuracy');
-const trainModelButton = document.getElementById('train-model');
+const collectDataButton = document.getElementById('collectData');
+const upgradeProcessingPowerButton = document.getElementById('upgradeProcessingPower');
+const trainModelButton = document.getElementById('trainModel');
 
-let dataPoints = 0;
-let modelAccuracy = 0;
+const dataCountElement = document.getElementById('dataCount');
+const processingPowerCountElement = document.getElementById('processingPowerCount');
+const accuracyElement = document.getElementById('accuracy');
+const dataProcessedElement = document.getElementById('dataProcessed');
 
-generateDataButton.addEventListener('click', () => {
-  dataPoints += Math.random() * 100;
-  dataPointsCountElement.textContent = `Data Points: ${dataPoints.toFixed(2)}`;
+let dataCount = 0;
+let processingPowerCount = 1;
+let accuracy = 0;
+let dataProcessed = 0;
+
+collectDataButton.addEventListener('click', () => {
+    if (dataCount > 0) {
+        dataCount--;
+        dataProcessed++;
+        updateResources();
+        updateProgressBar();
+    }
+});
+
+upgradeProcessingPowerButton.addEventListener('click', () => {
+    if (processingPowerCount > 0) {
+        processingPowerCount--;
+        accuracy += 0.1;
+        updateResources();
+        updateProgressBar();
+    }
 });
 
 trainModelButton.addEventListener('click', () => {
-  // Train the machine learning model
-  modelAccuracy = Math.random() * 100;
-  modelAccuracyElement.textContent = `${modelAccuracy.toFixed(2)}%`;
+    if (dataProcessed >= 1000 && accuracy >= 0.9) {
+        alert('You successfully trained the machine learning model!');
+    } else {
+        alert('You need more data and higher accuracy to train the model.');
+    }
 });
+
+function updateResources() {
+    dataCountElement.textContent = dataCount;
+    processingPowerCountElement.textContent = processingPowerCount;
+    accuracyElement.textContent = accuracy.toFixed(2);
+    dataProcessedElement.textContent = dataProcessed;
+}
+
+function updateProgressBar() {
+    const progressWidth = (dataProcessed / 10000) * 100;
+    document.querySelector('.progress').style.width = progressWidth + '%';
+}
